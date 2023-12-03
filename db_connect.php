79ec2da20fc7ec8aeec6a0730adc999dd9b9721b
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 08:11:24 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,8 +3,17 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:eesdb.database.windows.net,1433; Database = ees_db", "ryaned15", "Titleist5");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
+
+    // Example query
+    $sql = "SELECT * FROM system_settings";
+    $stmt = $conn->query($sql);
+
+    // Fetch data
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // Process the row data
+        print_r($row);
+    }
+} catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
