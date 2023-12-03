b9a4642c5261f715d03c7bdcdbf35e802a3b8e98
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 10:57:31 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,19 +3,14 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:eesdb.database.windows.net,1433; Database = ees_db", "ryaned15", "Titleist5");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Example query
-    $sql = "SELECT * FROM system_settings";
-    $stmt = $conn->query($sql);
-
-    // Fetch data
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // Process the row data
-        print_r($row);
-    }
-} catch (PDOException $e) {
+}
+catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ryaned15", "pwd" => "Titleist5", "Database" => "ees_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:eesdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
