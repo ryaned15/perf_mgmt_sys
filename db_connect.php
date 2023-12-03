ac325e05d618b03dfdd8507444cd80142394eda3
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 11:13:47 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -10,15 +10,4 @@ $conn = sqlsrv_connect($serverName, $connectionOptions);
 
 if ($conn === false) {
     die(print_r(sqlsrv_errors(), true));
-} else {
-    echo "Connected to Azure SQL Database!";
-    // Perform operations here
-    // Example: query execution
-    $query = "SELECT * FROM system_settings";
-    $stmt = sqlsrv_query($conn, $query);
-    // Handle query results
-    // ...
-}
 
-sqlsrv_close($conn);
-?>
