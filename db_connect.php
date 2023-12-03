786a14a78a78cef09b8e82cce6cc68932f439fef
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 11:02:33 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,24 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:eesdb.database.windows.net,1433; Database = ees_db", "ryaned15", "Titleist5");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+$serverName = "eesdb.database.windows.net";
+$connectionOptions = array(
+    "Database" => "ees_db",
+    "Uid" => "ryaned15",
+    "PWD" => "Titleist5"
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    echo "Connected to Azure SQL Database!";
+    // Perform operations here
+    // Example: query execution
+    $query = "SELECT * FROM system_settings";
+    $stmt = sqlsrv_query($conn, $query);
+    // Handle query results
+    // ...
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ryaned15", "pwd" => "Titleist5", "Database" => "ees_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:eesdb.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+sqlsrv_close($conn);
 ?>
