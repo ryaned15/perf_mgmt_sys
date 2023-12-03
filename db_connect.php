61ec057eea3daa95ab7a82ddb799cfabb322d545
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 11:38:05 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,17 @@
 <?php
 
-$conn= new mysqli('eesdb.database.windows.net', 'ryaned15', 'Titleist5', 'ees_db')or die("Could not connect to mysql".mysqli_error($con));
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:eesdb.database.windows.net,1433; Database = ees_db", "ryaned15", "Titleist5");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ryaned15", "pwd" => "Titleist5", "Database" => "ees_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:eesdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
