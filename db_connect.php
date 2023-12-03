135fff1809517d197b0cd7911d6d5d7f0e35c85c
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 12:20:57 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,20 @@
 <?php
-$serverName = "eesdb.database.windows.net";
-$connectionOptions = array(
-    "Database" => "ees_db",
-    "Uid" => "ryaned15",
-    "PWD" => "Titleist5"
-);
+function connectToDatabase() {
+    $serverName = "eesdb.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "ees_db",
+        "Uid" => "ryaned15",
+        "PWD" => "Titleist5"
+    );
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
-} else {
-   return $conn; 
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if ($conn === false) {
+        die(print_r(sqlsrv_errors(), true));
+    } else {
+        return $conn;
+    }
 }
-?>
 
+// To use the connection:
+$connection = connectToDatabase();
+?>
