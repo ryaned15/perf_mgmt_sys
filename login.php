294f19babfe97a088b5862902640b4e8e99da146
ryaned15 <130252774+ryaned15@users.noreply.github.com>
2023-12-03 13:35:18 -0700

    Update login.php

--- login.php
+++ login.php
@@ -3,15 +3,29 @@
 <?php 
 session_start();
 include('./db_connect.php');
-  ob_start();
-  // if(!isset($_SESSION['system'])){
+ob_start();
 
-    $system = $conn->query("SELECT * FROM system_settings")->fetch_array();
-    foreach($system as $k => $v){
-      $_SESSION['system'][$k] = $v;
+// Ensure the connection is successful before executing the query
+if ($conn) {
+    $result = $conn->query("SELECT * FROM system_settings");
+    
+    if ($result) {
+        while ($row = $result->fetch_assoc()) {
+            foreach ($row as $k => $v) {
+                $_SESSION['system'][$k] = $v;
+            }
+        }
+    } else {
+        // Handle the query execution failure
+        // For example:
+        die("Query execution failed: " . $conn->error);
     }
-  // }
-  ob_end_flush();
+} else {
+    // Handle connection failure
+    die("Connection failed: " . $conn->connect_error);
+}
+
+ob_end_flush();
 ?>
 <?php 
 if(isset($_SESSION['login_id']))
